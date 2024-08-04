0e568d6bec65bfd3d3b1de6de14f1449c08ee138
LTP <126170288+LTPPPP@users.noreply.github.com>
2024-08-05 01:41:25 +0700

    Update geminiBot.py

--- python/geminiBot.py
+++ python/geminiBot.py
@@ -29,7 +29,7 @@ r = Rake()
 app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 # Set up Gemini API
-genai.configure(api_key='AIzaSyABvqN-8d3jpqlOeE1HzSK07LcW-R1B0Ss')
+genai.configure(api_key='API_KEY')
 model = genai.GenerativeModel('gemini-1.5-pro')
 
 UPLOAD_FOLDER = 'uploads'
